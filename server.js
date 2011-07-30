96a937ff2b60b6fdbcb65d6f56088ec7759ac7b0
Aurélien Schaller <wyweb.fr@gmail.com>
2011-07-30 08:18:45 -0700

    rererer

--- server.js
+++ server.js
@@ -4,12 +4,11 @@ var http = require('http');
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World test cloud9\n");
+  response.end("Hello World test\n");
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(8000);
+server.listen(process.env.C9_PORT);
 
 // Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:8000/");
-
+console.log("Server running at 0.0.0.0");
