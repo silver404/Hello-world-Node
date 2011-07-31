c2206df24f76f1a25e5cb537fa2f0733fdac6367
Aurélien Schaller <wyweb.fr@gmail.com>
2011-07-31 02:49:27 -0700

    test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var server = http.createServer(function (request, response) {
 });
 
 // Listen on port 8000, IP defaults to 127.0.0.1
-server.listen(process.env.C9_PORT);
+server.listen("8080");
 
 // Put a friendly message on the terminal
 console.log("Server running at 0.0.0.0");
